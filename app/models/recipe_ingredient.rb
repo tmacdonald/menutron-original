3a177a05b48bcc266064cc6d31e2c946f796f2ac
Tim Macdonald <tmacdonald@smarttech.com>
2011-03-06 15:30:03 -0500

    Worked on ingredient parsing

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,15 +3,26 @@ class RecipeIngredient < ActiveRecord::Base
   belongs_to :measurement
   belongs_to :recipe
 
-  def how_much
-  end
+  attr_accessor :how_much, :what
 
-  def how_much=(value)
-  end
+  validates_presence_of :what
 
-  def what
-  end
+  before_save :parse_virtual_attributes
 
-  def what=(value)
+  private
+
+  def parse_virtual_attributes
+    if @how_much
+	tokens = @how_much.split(/\s+/)
+
+	logger.info tokens[0]
+	logger.info tokens[1]
+
+	self.amount = tokens[0]
+	self.measurement_id = tokens[1]
+	#self.measurement = Measurement.find_by_name(tokens[1])
+    end
+
+    self.ingredient = Ingredient.find_or_create_by_name(@what)
   end
 end
