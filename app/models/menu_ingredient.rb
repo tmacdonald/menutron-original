2deb4a8a86dba74bd7f8cc5951f8c2ead9ed992b
Tim Macdonald <tmacdonald@smarttech.com>
2011-03-14 13:36:41 -0400

    Moved before_save functionality of menu ingredients and menu recipes to observers

--- app/models/menu_ingredient.rb
+++ app/models/menu_ingredient.rb
@@ -7,15 +7,5 @@ class MenuIngredient < ActiveRecord::Base
 
   before_save :parse_virtual_attributes
   after_find :populate_virtual_attributes
-  after_save :copy_to_groceries
 
-  def copy_to_groceries
-    grocery = Grocery.new
-    grocery.menu_id = self.menu_id
-    grocery.menu_ingredient_id = self.id
-    grocery.how_much = self.how_much
-    grocery.ingredient_name = self.ingredient_name
-    grocery.save
-    self.grocery = grocery
-  end
 end
--- app/models/menu_recipe.rb
+++ app/models/menu_recipe.rb
@@ -2,17 +2,4 @@ class MenuRecipe < ActiveRecord::Base
   belongs_to :menu
   belongs_to :recipe
   has_many :groceries
-
-  after_save :copy_to_groceries
-  
-  def copy_to_groceries
-    self.recipe.ingredients.each do |i|
-      grocery = Grocery.new
-      grocery.menu_id = self.menu_id
-      grocery.menu_recipe_id = self.id
-      grocery.how_much = i.how_much
-      grocery.ingredient_name = i.ingredient_name
-      grocery.save
-    end
-  end
 end
