9b83fb9251a0deb9cb2ca002e518627ad13f0e1b
Tim Macdonald <tmacdonald@smarttech.com>
2011-04-01 14:22:07 -0400

    Updated ingredients and measurements functional tests to test json requests

--- test/functional/ingredients_controller_test.rb
+++ test/functional/ingredients_controller_test.rb
@@ -11,6 +11,12 @@ class IngredientsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:ingredients)
   end
 
+  test "should get index json" do
+    get :index, :format => "json"
+    assert_response :success
+    assert_not_nil assigns(:ingredients)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -24,9 +30,23 @@ class IngredientsControllerTest < ActionController::TestCase
     assert_redirected_to ingredient_path(assigns(:ingredient))
   end
 
+  test "should create ingredient json" do
+    assert_difference('Ingredient.count') do
+      post :create, :ingredient => @ingredient.attributes, :format => "json"
+    end
+  
+    assert_response :success
+  end
+
   test "should show ingredient" do
     get :show, :id => @ingredient.to_param
     assert_response :success
+  end
+
+  test "should show ingredient json" do
+    get :show, :id => @ingredient.to_param, :format => "json"
+    assert_response :success
+    assert_not_nil assigns(:ingredient)
   end
 
   test "should get edit" do
@@ -39,11 +59,24 @@ class IngredientsControllerTest < ActionController::TestCase
     assert_redirected_to ingredient_path(assigns(:ingredient))
   end
 
+  test "should update ingredient using json" do
+    put :update, :id => @ingredient.to_param, :ingredient => @ingredient.attributes, :format => "json"
+    assert_response :success
+  end
+
   test "should destroy ingredient" do
     assert_difference('Ingredient.count', -1) do
       delete :destroy, :id => @ingredient.to_param
     end
 
     assert_redirected_to ingredients_path
+  end
+
+  test "should destroy ingredient using json" do
+    assert_difference('Ingredient.count', -1) do
+      delete :destroy, :id => @ingredient.to_param, :format => "json"
+    end
+
+    assert_response :success
   end
 end
--- test/functional/measurements_controller_test.rb
+++ test/functional/measurements_controller_test.rb
@@ -11,6 +11,12 @@ class MeasurementsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:measurements)
   end
 
+  test "should get index using json" do
+    get :index, :format => "json"
+    assert_response :success
+    assert_not_nil assigns(:measurements)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -24,9 +30,23 @@ class MeasurementsControllerTest < ActionController::TestCase
     assert_redirected_to measurement_path(assigns(:measurement))
   end
 
+  test "should create measurement using json" do
+    assert_difference('Measurement.count') do
+      post :create, :measurement => @measurement.attributes, :format => "json"
+    end
+
+    assert_response :success
+  end
+
   test "should show measurement" do
     get :show, :id => @measurement.to_param
     assert_response :success
+  end
+
+  test "should show measurement using json" do
+    get :show, :id => @measurement.to_param, :format => "json"
+    assert_response :success
+    assert_not_nil assigns(:measurement)
   end
 
   test "should get edit" do
@@ -39,11 +59,24 @@ class MeasurementsControllerTest < ActionController::TestCase
     assert_redirected_to measurement_path(assigns(:measurement))
   end
 
+  test "should update measurement using json" do
+    put :update, :id => @measurement.to_param, :measurement => @measurement.attributes, :format => "json"
+    assert_response :success
+  end
+
   test "should destroy measurement" do
     assert_difference('Measurement.count', -1) do
       delete :destroy, :id => @measurement.to_param
     end
 
     assert_redirected_to measurements_path
+  end
+
+  test "should destroy measurement using json" do
+    assert_difference('Measurement.count', -1) do
+      delete :destroy, :id => @measurement.to_param, :format => "json"
+    end
+
+    assert_response :success
   end
 end
