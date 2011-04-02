adc378e722189b41010c28c93361a9458c4748b2
Tim Macdonald <tmacdonald@smarttech.com>
2011-04-02 10:18:37 -0400

    Created unit tests for recipe_ingredient, menu_ingredients, and groceries

--- test/unit/grocery_test.rb
+++ test/unit/grocery_test.rb
@@ -1,8 +1,70 @@
 require 'test_helper'
 
 class GroceryTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    @menu = menus(:one)
+  end
+  
+  test "should create grocery using existing measurement and ingredient" do
+    grocery = @menu.groceries.build
+
+    grocery.how_much = "1 cup"
+    grocery.ingredient_name = "blueberries"
+    assert grocery.save
+
+    assert_equal ingredients(:blueberries).id, grocery.ingredient_id
+    assert_equal measurements(:cups).id, grocery.measurement_id
+    assert_equal 1, grocery.amount
+  end
+
+  test "should create grocery with new ingredient" do
+    grocery = @menu.groceries.build
+  
+    grocery.how_much = "1 cup"
+    grocery.ingredient_name = "raspberries"
+    assert grocery.save
+
+    raspberries = Ingredient.find_by_name("raspberries")
+    assert_not_nil raspberries
+
+    assert_equal raspberries.id, grocery.ingredient_id
+    assert_equal measurements(:cups).id, grocery.measurement_id
+    assert_equal 1, grocery.amount
+  end
+
+  test "should create grocery with no measurement" do
+    grocery = @menu.groceries.build
+    
+    grocery.how_much = "1"
+    grocery.ingredient_name = "watermelon" 
+    assert grocery.save
+
+    assert_equal ingredients(:watermelon).id, grocery.ingredient_id
+    assert_nil grocery.measurement_id
+    assert_equal 1, grocery.amount
+  end
+
+  test "should create grocery with simple fraction" do
+    grocery = @menu.groceries.build
+    
+    grocery.how_much = "1/2"
+    grocery.ingredient_name = "watermelon"
+    assert grocery.save
+
+    assert_equal ingredients(:watermelon).id, grocery.ingredient_id
+    assert_nil grocery.measurement_id
+    assert_equal 0.5, grocery.amount
+  end
+
+  test "should create grocery with mixed fraction" do
+    grocery = @menu.groceries.build
+    
+    grocery.how_much = "1 1/2"
+    grocery.ingredient_name = "watermelon"
+    assert grocery.save
+
+    assert_equal ingredients(:watermelon).id, grocery.ingredient_id
+    assert_nil grocery.measurement_id
+    assert_equal 1.5, grocery.amount
   end
 end
--- test/unit/menu_ingredient_test.rb
+++ test/unit/menu_ingredient_test.rb
@@ -1,8 +1,84 @@
 require 'test_helper'
 
 class MenuIngredientTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    @menu = menus(:one)
+  end
+  
+  test "should create menu ingredient using existing measurement and ingredient" do
+    menu_ingredient = @menu.ingredients.build
+
+    menu_ingredient.how_much = "1 cup"
+    menu_ingredient.ingredient_name = "blueberries"
+    assert menu_ingredient.save
+
+    assert_equal ingredients(:blueberries).id, menu_ingredient.ingredient_id
+    assert_equal measurements(:cups).id, menu_ingredient.measurement_id
+    assert_equal 1, menu_ingredient.amount
+  end
+
+  test "should create menu ingredient with new ingredient" do
+    menu_ingredient = @menu.ingredients.build
+  
+    menu_ingredient.how_much = "1 cup"
+    menu_ingredient.ingredient_name = "raspberries"
+    assert menu_ingredient.save
+
+    raspberries = Ingredient.find_by_name("raspberries")
+    assert_not_nil raspberries
+
+    assert_equal raspberries.id, menu_ingredient.ingredient_id
+    assert_equal measurements(:cups).id, menu_ingredient.measurement_id
+    assert_equal 1, menu_ingredient.amount
+  end
+
+  test "should create menu ingredient with no measurement" do
+    menu_ingredient = @menu.ingredients.build
+    
+    menu_ingredient.how_much = "1"
+    menu_ingredient.ingredient_name = "watermelon" 
+    assert menu_ingredient.save
+
+    assert_equal ingredients(:watermelon).id, menu_ingredient.ingredient_id
+    assert_nil menu_ingredient.measurement_id
+    assert_equal 1, menu_ingredient.amount
+  end
+
+  test "should create menu ingredient with simple fraction" do
+    menu_ingredient = @menu.ingredients.build
+    
+    menu_ingredient.how_much = "1/2"
+    menu_ingredient.ingredient_name = "watermelon"
+    assert menu_ingredient.save
+
+    assert_equal ingredients(:watermelon).id, menu_ingredient.ingredient_id
+    assert_nil menu_ingredient.measurement_id
+    assert_equal 0.5, menu_ingredient.amount
+  end
+
+  test "should create menu ingredient with mixed fraction" do
+    menu_ingredient = @menu.ingredients.build
+    
+    menu_ingredient.how_much = "1 1/2"
+    menu_ingredient.ingredient_name = "watermelon"
+    assert menu_ingredient.save
+
+    assert_equal ingredients(:watermelon).id, menu_ingredient.ingredient_id
+    assert_nil menu_ingredient.measurement_id
+    assert_equal 1.5, menu_ingredient.amount
+  end
+
+  test "should create menu ingredient with preparation" do
+    menu_ingredient = @menu.ingredients.build
+    
+    menu_ingredient.how_much = "1"
+    menu_ingredient.ingredient_name = "watermelon" 
+    menu_ingredient.preparation = "halved"
+    assert menu_ingredient.save
+
+    assert_equal ingredients(:watermelon).id, menu_ingredient.ingredient_id
+    assert_nil menu_ingredient.measurement_id
+    assert_equal 1, menu_ingredient.amount
+    assert_equal "halved", menu_ingredient.preparation
   end
 end
