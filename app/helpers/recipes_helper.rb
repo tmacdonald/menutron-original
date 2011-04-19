1d5afa14e72bb5103f38208197129c52102adc89
Tim Macdonald <macdonald.tim@gmail.com>
2011-04-18 21:04:31 -0400

    Looking at a different way of adding directions using javascript

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,11 @@
 module RecipesHelper
+  def add_direction_link(name, form)
+    link_to_function name do |page|
+      direction = render(:partial => "direction", :locals => { :form => form, :direction => RecipeDirection.new })
+      page << %{
+        var new_direction_id = "new_" + new Date().getTime();
+        $('#directions').append( "#{ escape_javascript direction }".replace(/new_\\d+/g, new_direction_id) );
+      }
+    end
+  end
 end
