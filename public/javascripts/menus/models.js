513a525e1b986c1d21ccfa09bf34d2455cbef248
Tim Macdonald <macdonald.tim@gmail.com>
2011-07-15 16:25:36 -0400

    Implemented display of menu recipes

--- public/javascripts/menus/models.js
+++ public/javascripts/menus/models.js
@@ -10,13 +10,78 @@ var RecipeView = Backbone.View.extend({
   tagName: 'li',
 
 	render: function() {
-		var template ='<div id="recipe_{{cid}}"><span class="name">{{name}}</span></div>';
+    var template = $('#recipe_template').html();
 		var context = _.extend( this.model.toJSON(), { cid: this.model.cid } );
-		$(this.el).html( Mustache.to_html( template, context ) );
+		$(this.el).attr( 'id', this.model.id ).html( Mustache.to_html( template, context ) );
 		return this;
 	}
 });
 var recipes = new RecipeCollection();
+
+var MenuRecipe = Backbone.Model.extend({
+});
+
+var MenuRecipes = Backbone.Collection.extend({
+  model: MenuRecipe
+});
+
+var MenuRecipeView = Backbone.View.extend({
+  tagName: "li",
+
+  render: function() {
+    var template = $('#menu_recipe_template').html();
+    var context = _.extend( this.model.toJSON(), { cid: this.model.cid } );
+    $(this.el).html( Mustache.to_html( template, context ) ).attr( 'id', this.model.id );
+    return this;
+  } 
+
+});
+
+var MenuIngredient = Backbone.Model.extend({
+});
+
+var MenuIngredients = Backbone.Collection.extend({
+  model: MenuIngredient
+});
+
+var Menu = Backbone.Model.extend({
+  initialize: function( attrs ) {
+    this.recipes = new MenuRecipes;
+    this.recipes.url = '/menus/' + this.id + '/recipes';
+    this.recipes.reset( attrs.recipes );
+
+    this.ingredients = new MenuIngredients;
+    this.ingredients.url = '/menus/' + this.id + '/ingredients';
+    this.ingredients.reset( attrs.ingredients );
+  }
+});
+var menu = null;
+
+var MenuView = Backbone.View.extend({
+  el: $('#menu'),
+
+  initialize: function( attrs ) {
+    _.bindAll( this, 'addOneRecipe', 'addAllRecipes', 'render' );
+
+    this.model.recipes.bind( 'add', this.addOneRecipe );
+    this.model.recipes.bind( 'reset', this.addAllRecipes );
+    this.model.recipes.bind( 'all', this.render );
+    this.addAllRecipes();
+
+    //attrs.menu.ingredients.bind( 'add', this.addOneIngredient );
+    //attrs.menu.ingredients.bind( 'reset', this.addAllIngredients );
+    //attrs.menu.ingredients.bind( 'all', this.render );
+  },
+
+  addOneRecipe: function( recipe ) {
+    var view = new MenuRecipeView( { model: recipe } );
+    $('#menu_recipes').append( view.render().el );
+  },
+
+  addAllRecipes: function( ) {
+    this.model.recipes.each( this.addOneRecipe );
+  }
+});
 
 var AppView = Backbone.View.extend({
   el: $('#app'),
@@ -43,41 +108,4 @@ var AppView = Backbone.View.extend({
   }
 });
 
-var MenuRecipe = Backbone.Model.extend({
-});
-
-var MenuRecipes = Backbone.Collection.extend({
-  model: MenuRecipe
-});
-
-var MenuIngredient = Backbone.Model.extend({
-});
-
-var MenuIngredients = Backbone.Collection.extend({
-  model: MenuIngredient
-});
-
-var Menu = Backbone.Model.extend({
-  initialize: function( attrs ) {
-    this.recipes = new MenuRecipes;
-    this.recipes.url = '/menus/' + this.id + '/recipes';
-    this.recipes.reset( attrs.recipes );
-
-    this.ingredients = new MenuIngredients;
-    this.ingredients.url = '/menus/' + this.id + '/ingredients';
-    this.ingredients.reset( attrs.ingredients );
-  }
-});
-var menu = null;
-
 window.App = new AppView();
-
-/*
-var AppRouter = Backbone.Router.extend({
-	initialize: function( params ) {
-    this.model = new AppModel();
-    this.view = new AppView( { model: this.model } );
-    params.append_at.append( this.view.render().el );
-	}
-});
-*/
