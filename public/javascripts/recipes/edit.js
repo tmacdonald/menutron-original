b15ec854eef318ff3420b7e94433c4cd43ee1e34
Tim Macdonald <macdonald.tim@gmail.com>
2011-10-06 08:11:19 -0400

    Implemented some UX ideas for recipe directions. TODO: Clean it up.

--- public/javascripts/recipes/edit.js
+++ public/javascripts/recipes/edit.js
@@ -1,7 +1,89 @@
 $(document).ready( function() {
 
-  $('.destroy').hide();
+  //$('.destroy').hide();
+  $('.direction.new').hide();
 
+  function generate_direction_template() {
+    var $template = $('<div></div>');
+    $template.html( $('.direction:first').html().replace(/_0_/g, '_{index}_').replace(/\[0\]/g, '[{index}]') );
+    return $template;
+  }
+
+  function generate_ingredient_template() {
+    var $template = $('<div></div>');
+    $template.html( $('.ingredient:first').html().replace(/_0_/g, '_{index}_').replace(/\[0\]/g, '[{index}]') );
+    return $template;
+  }
+
+  function create_new_direction( $direction_template, direction ) {
+    var index = $('.direction').length;
+    var $direction = $('<div class="direction"></div>');
+    $direction.html( $direction_template.html().replace(/\{index\}/g, index) );
+    $direction.find('.text').val( direction.text );
+    return $direction;
+  }
+
+  function create_new_ingredient( $ingredient_template, ingredient ) {
+    var index = $('.ingredient').length;
+    var $ingredient = $('<div class="ingredient"></div>');
+    $ingredient.html( $ingredient_template.html().replace(/\{index\}/g, index) );
+    $ingredient.find('.how_much').val( ingredient.how_much );
+    $ingredient.find('.ingredient_name').val( ingredient.ingredient_name );
+    $ingredient.find('.preparation').val( ingredient.preparation );
+    return $ingredient;
+  }
+
+  $('#directions').delegate( '.direction .view .edit_link', 'click', function( e ) {
+    e.preventDefault();
+
+    var $view = $(e.target).closest('.view').hide();
+    var $edit = $view.parent().find('.edit').show();
+  });
+
+  $('#directions').delegate( '.direction .view .delete_link', 'click', function( e ) {
+    e.preventDefault();
+
+    var $view = $(e.target).closest('.view');
+    $view.closest('.direction').hide();
+    var $edit = $view.parent().find('.edit');
+
+    $edit.find('.destroy').attr('checked', 'checked');
+  });
+
+  $('#directions').delegate( '.direction .edit button', 'click', function( e ) {
+    e.preventDefault();
+
+    var $edit = $(e.target).closest('.edit').hide();
+    var $view = $edit.parent().find('.view').show();
+  });
+
+  $('#directions').delegate( '.direction .edit .text', 'keyup', function( e ) {
+    $view_div = $(e.target).closest('.edit').parent().find('.view');
+    $view_div.html( Mustache.to_html( $('#direction_template').html(), { text: $(e.target).val() } ) );
+  });
+
+  $('.direction:not(.new)').each( function( i, e ) {
+    make_direction_editable( $(e) );
+  });
+
+  function make_direction_editable( $target ) {
+    var $edit_div = $('<div class="edit"></div>');
+    var $view_div = $('<div class="view"></div>');
+    $edit_div.html( $target.html() ).append('<button>Save</button>').hide();
+    $view_div.html( Mustache.to_html( $('#direction_template').html(), { text: $target.find('.text').html() } ) );
+    $target.html( '' ).append( $view_div ).append( $edit_div );
+  }
+
+  var $direction_template = generate_direction_template();
+  
+  //var $direction = create_new_direction( $direction_template, { text: 'This is another direction' } );
+  //$('#directions').append( $direction );
+
+  var $ingredient_template = generate_ingredient_template();
+  
+  //var $ingredient = create_new_ingredient( $ingredient_template, { how_much: '1 1/2 cups', ingredient_name: 'carrots', preparation: 'shredded' } );
+  //$('#ingredients').append( $ingredient );
+  
   /*
   var new_direction_template = $("#new_direction_template").html();
   var new_ingredient_template = $("#new_ingredient_template").html();
